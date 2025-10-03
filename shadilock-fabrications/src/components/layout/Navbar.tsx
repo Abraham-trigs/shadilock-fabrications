3c9ddbb12136eb0833090e69c49b1feb72b7231f
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-10-03 14:09:27 +0100

    book appoint,ment margin-top increased in mobile view

--- shadilock-fabrications/src/components/layout/Navbar.tsx
+++ shadilock-fabrications/src/components/layout/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
               </Link>
             ))}
             <Link href="/book"
-              <button className=" mt-8 px-4 py-2 bg-orange text-blue font-semibold rounded-lg shadow-md hover:bg-orangeHover transition-transform transform hover:scale-105 active:scale-95">
+              <button className=" mt-12 px-4 py-2 bg-orange text-blue font-semibold rounded-lg shadow-md hover:bg-orangeHover transition-transform transform hover:scale-105 active:scale-95">
                 Book an Appointment
               </button>
             </Link>{" "}
